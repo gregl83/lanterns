d876f8ac252f6ab35d84b7307d594fbeb2b00249
Gregory Langlais <general@gregorylanglais.com>
2020-11-20 10:21:56 -0500

    add io adapter trais

--- src/io/adapters/mod.rs
+++ src/io/adapters/mod.rs
@@ -4,10 +4,24 @@ use std::error::Error;
 
 pub use bluetooth::*;
 
-pub trait Connectable {
-    fn discover_devices() -> Result<Vec<String>, Box<dyn Error>>;
+pub trait Discoverable {
+    fn discover_devices() -> Result<Vec<dyn Connectable>, Box<dyn Error>>;
+}
 
-    fn connect() -> Result<(), Box<dyn Error>>;
+pub trait Connectable {
+    type Key;
+
+    fn connect(key: key) -> Result<(), Box<dyn Error>>;
+}
+
+pub struct Device { }
+
+impl Connectable for Device {
+    type Key = str;
+
+    fn connect(key: Key) -> Result<(), Box<dyn Error>> {
+        // fixme
+    }
 }
 
 // todo - make struct for bluetooth using traits
