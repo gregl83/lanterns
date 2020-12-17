b5566913d6c293f73f578ef251693b48d4d0f3ef
Gregory Langlais <general@gregorylanglais.com>
2020-12-17 11:24:55 -0500

    update dashboard welcome for valign

--- src/gui/modules/welcome.rs
+++ src/gui/modules/welcome.rs
@@ -13,23 +13,28 @@ use tui::{
         Wrap
     },
     text::{
+        Text,
         Spans,
         Span
     }
 };
 
 pub fn draw_welcome_message(height: u16) -> Paragraph<'static> {
-    let text = vec![
+    let midpoint = height / 2;
+    let start_point = if height > 10 { midpoint - 5 } else { 0 };
+    let padding_top = "\r\n".repeat(start_point as usize);
+    let mut text = Text::raw(padding_top).lines.clone();
+    text.append(&mut vec![
         Spans::from(vec![
-            Span::raw("Lanterns"),
+            Span::raw("-  L A N T E R N S  -"),
         ]),
         Spans::from(vec![
-            Span::raw("")
+            Span::raw(""),
         ]),
         Spans::from(vec![
             Span::styled("Private Communications No Holds Barred",Style::default().add_modifier(Modifier::ITALIC)),
         ]),
-    ];
+    ]);
     Paragraph::new(text)
         .block(Block::default().borders(Borders::ALL))
         .style(Style::default().fg(Color::White).bg(Color::Black))
